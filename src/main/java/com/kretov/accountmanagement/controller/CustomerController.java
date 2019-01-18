f9d9bbacab765acfae0a2db4b3f556d4af1ed9f6
Kretov Evgeny <e.a.kretov@gmail.com>
2019-01-18 23:25:11 +0300

    Renamed UserController into CustomerController

--- src/main/java/com/kretov/accountmanagement/controller/CustomerController.java
+++ src/main/java/com/kretov/accountmanagement/controller/CustomerController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-public class UserController {
+public class CustomerController {
 
     @Autowired
     private CustomerService customerService;
